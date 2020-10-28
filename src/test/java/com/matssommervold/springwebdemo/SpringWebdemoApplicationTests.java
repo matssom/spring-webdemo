2f0b5e525ed31c42ba653ff1992d22da2e3afafa
Mats Sommervold <mats.sommervold@gmail.com>
2020-10-28 11:08:26 +0100

    Initial commit

---
+++ src/test/java/com/matssommervold/springwebdemo/SpringWebdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matssommervold.springwebdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
